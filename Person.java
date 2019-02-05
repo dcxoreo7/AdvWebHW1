bf64c68f8bd03025d8bd49b4501da0644e43ced1
dcxoreo7 <31734649+dcxoreo7@users.noreply.github.com>
2019-02-04 22:20:32 -0600

    These are the main files that I edited and created for lab 3
    , I believe I had the right JSon code and everything but when I tried to redirect the servlet to index.jsp I got a huge error, kind of like the one we exprerienced with the indent during office hours but almost twice as long and I dont really know what I did wrong or what IS wrong, I did the lab over again and ran into the same error so I will just post this.

---
+++ Person.java
@@ -0,0 +1,17 @@
+package edu.simpson.dc;
+
+public class Person {
+    private int id;
+    private String first;
+    private String last;
+    private String phone;
+
+    public int getId() {return id; }
+    public void setId(int id) { this.id = id; }
+
+    public String getFirst() {return first; }
+    public void setFirst(String first) {this.first = first; }
+
+    public String getLast() {return last; }
+    public void setLast(String last) {this.last = last; }
+}
